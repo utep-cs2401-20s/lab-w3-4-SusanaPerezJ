4b6fefdec36f3ed4b42cec940c6362d6042bfb97
Susana Perez Juarez <sperezjuar1@miners.utep.edu>
2020-02-05 13:46:51 -0700

    Test commit

---
+++ Lab 3/src/GameOfLife.java
@@ -0,0 +1,15 @@
+public class GameOfLife {
+    private int size;
+    private int[][] board;
+    private int[][] previous;
+
+    public GameOfLife(){
+
+    }
+    public GameOfLife(int size){
+        this.size = size;
+    }
+    public GameOfLife(int[][] board){
+
+    }
+}
